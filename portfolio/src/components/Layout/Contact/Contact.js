89f5ec946eabde64c8d42262ed0c2ef738464291
mrchappie <boscu20@gmail.com>
2022-10-08 20:59:21 +0300

    linked contact form to db

--- portfolio/src/components/Layout/Contact/Contact.js
+++ portfolio/src/components/Layout/Contact/Contact.js
@@ -6,8 +6,32 @@ import Card from '../../UI/Card/Card';
 
 import BasicInfo from '../About/Skills/BasicInfo';
 
+import useHttp from '../../../../src/hooks/use-http';
+
 const Contact = () => {
   // const activeColor = useSelector((state) => state.activeColor.color);
+
+  const getInputData = (dataReceived) => {
+    // console.log(dataReceived);
+    sendDataRequest(dataReceived);
+  };
+
+  const { isLoading, error, sendRequest: sendData } = useHttp();
+
+  const sendDataRequest = async (dataToBeSent) => {
+    sendData({
+      url: 'https://portfolio-chappie-default-rtdb.europe-west1.firebasedatabase.app/messages.json',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: dataToBeSent.name,
+        email: dataToBeSent.email,
+        message: dataToBeSent.message,
+      }),
+    });
+  };
 
   return (
     <div className={classes['contact-container']}>
@@ -15,7 +39,10 @@ const Contact = () => {
         <div className={classes.about}>
           <BasicInfo></BasicInfo>
         </div>
-        <Input className={classes.form}></Input>
+        <Input className={classes.form} onGetInputData={getInputData}></Input>
+        <div className={classes.error}>
+          {isLoading && <p>{error ? error : 'Your message is on my way...'}</p>}
+        </div>
       </Card>
     </div>
   );
--- portfolio/src/components/UI/Input/Input.js
+++ portfolio/src/components/UI/Input/Input.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Button from '../Button/Button/Button';
 import classes from './Input.module.css';
@@ -5,15 +6,45 @@ import classes from './Input.module.css';
 const Input = (props) => {
   const activeColor = useSelector((state) => state.activeColor.color);
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendInputData = (event) => {
+    event.preventDefault();
+
+    props.onGetInputData({
+      name: name,
+      email: email,
+      message: message,
+    });
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
     <div className={props.className}>
-      <form className={classes['form-container']}>
+      <form className={classes['form-container']} onSubmit={sendInputData}>
         <div className={classes['top-info']}>
           <div className={classes.name}>
-            <input type="text" id="name" placeholder="Name" />
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              onChange={(event) => setName(event.target.value)}
+              value={name}
+            />
           </div>
           <div className={classes.email}>
-            <input type="text" id="name" placeholder="Email" />
+            <input
+              type="text"
+              id="name"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+              value={email}
+            />
           </div>
         </div>
         <div className={classes.message}>
@@ -23,9 +54,13 @@ const Input = (props) => {
             cols="30"
             rows="10"
             placeholder="Message"
+            onChange={(event) => setMessage(event.target.value)}
+            value={message}
           ></textarea>
         </div>
-        <Button style={{ backgroundColor: activeColor }}>Contact Me</Button>
+        <Button type="submit" style={{ backgroundColor: activeColor }}>
+          Contact Me
+        </Button>
       </form>
     </div>
   );
